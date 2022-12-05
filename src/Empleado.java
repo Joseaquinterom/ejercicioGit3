0754f9e3da9adb0679c3b3992d804b9102dced11
José Antonio Quintero Maya <joseaquinterom@gmail.com>
2022-12-05 12:51:27 +0100

    Añadiendo código para probar las propiedades de Java

---
+++ src/Empleado.java
@@ -0,0 +1,18 @@
+/**
+ * @Author: José Antonio Quintero Maya
+ */
+class Empleado {
+    String nombre;
+    int edad;
+
+    Empleado(String name, int edad) {
+        nombre = name;
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "El empleado " + nombre + " tiene" + edad + "años";
+    }
+
+}
